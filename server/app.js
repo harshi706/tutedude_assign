19a09a7af7ae05b7fbe650c76a962b8657e85e4e
Harshi Agrawal <114180714+harshi706@users.noreply.github.com>
2025-01-05 20:10:34 +0530

    cors error

--- server/app.js
+++ server/app.js
@@ -3,11 +3,23 @@ import cors from 'cors'
 import cookieParser from 'cookie-parser';
 
 const app=express();
+const allowedOrigins = [
+  'https://tutedude-assign-1.onrender.com',
+  'http://localhost:5173'  //for development
+];
 app.use(cors({
-    origin: 'https://tutedude-assign-1.onrender.com',
-  credentials: true,
+  origin: (origin, callback) => {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true,  // To allow cookies
 }));
 
+// Handle preflight requests for all routes
+app.options('*', cors());
 app.use(express.json())
 app.use(express.urlencoded())
 app.use(cookieParser())
