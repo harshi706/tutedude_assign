2eeabb602be64f8180b3b86dd9f6541bdb58b8b2
Harshi Agrawal <114180714+harshi706@users.noreply.github.com>
2025-01-05 19:53:40 +0530

    Update Login.jsx

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -16,7 +16,7 @@ const Login = () => {
   const handleSubmit=async(e)=>{
     e.preventDefault();
     try{
-      const response=await axios.post("http://localhost:8000/auth/login",formData,{
+      const response=await axios.post("https://tutedude-assign.onrender.com/auth/login",formData,{
         withCredentials: true, //for cookies
         headers: {
           "Content-Type": "application/json",
