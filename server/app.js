b7c0fd82ecbdbedf427a0cc217911fe8152db7c4
Harshi Agrawal <114180714+harshi706@users.noreply.github.com>
2025-01-05 20:06:06 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -4,7 +4,7 @@ import cookieParser from 'cookie-parser';
 
 const app=express();
 app.use(cors({
-    origin: 'https://tutedude-assign-1.onrender.com/',
+    origin: 'https://tutedude-assign-1.onrender.com',
   credentials: true,
 }));
 
